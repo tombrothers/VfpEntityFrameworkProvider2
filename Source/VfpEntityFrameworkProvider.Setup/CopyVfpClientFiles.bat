482ee6db284ddef50ea711995a1d9749bfdd70e6
Tom Brothers <TomBrothers@outlook.com>
2019-11-10 07:35:20 -0500

    Removed installler.

--- Source/VfpEntityFrameworkProvider.Setup/CopyVfpClientFiles.bat
+++
@@ -1,8 +0,0 @@
-
-setlocal
-cd..
-cd..
-
-copy /y "%CD%\Source\Resources\net40\VfpClient*.dll" %CD%\Source\VfpEntityFrameworkProvider.Setup\Resources\net40
-copy /y "%CD%\Source\Resources\net45\VfpClient*.dll" %CD%\Source\VfpEntityFrameworkProvider.Setup\Resources\net45
-endlocal
